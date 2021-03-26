a3b7621c575bce97892e613d72eed80c266bd26a
kitoshi <kitoshi.charlton@gmail.com>
2021-03-26 12:00:44 -0700

    1.12*: anecdotes step1

---
+++ part 1.12/src/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+
+const Button = (props) => {
+  const setToSelected = (newSelected) => {props.setSelected(newSelected)}
+  const random = () => Math.floor(Math.random() * (5 - 0) + 0)
+
+  return (
+    <>
+    <button onClick={() => setToSelected(random)}>Next Anecdote</button>
+    </>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <p>{props.anecdotes[props.selected]}</p>
+  )
+}
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+
+  return (
+    <div>
+      <Content anecdotes={anecdotes} selected={selected} />
+      <Button setSelected={setSelected} selected={selected}/>
+    </div>
+  )
+}
+
+export default App
