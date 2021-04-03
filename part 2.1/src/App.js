fea1f89b0c213fdac6a2d5750325ef042f6e6795
kitoshi <kitoshi.charlton@gmail.com>
2021-04-03 11:45:57 -0700

    2.1: Course information step6

---
+++ part 2.1/src/App.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Course from './components/Course'
+
+const App = () => {
+
+}
+
+export default App;
---
+++ part 2.1/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Course from './components/Course'
+
+
+
+const App = () => {
+
+  return (
+    <Course />
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
