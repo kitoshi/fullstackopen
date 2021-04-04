5f136d2744be5b33eeb9648d29ffdc8f95146385
kitoshi <kitoshi.charlton@gmail.com>
2021-04-04 10:58:08 -0700

    2.2: Course information step7

--- part 2.1/src/components/Total.js
+++ part 2.1/src/components/Total.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
 const Total = ({ course }) => {
-    const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
-    return(
-        <p>Number of exercises {sum}</p>
+    const reducer = course.parts.reduce( 
+        (accumulator, currentValue) => accumulator + currentValue.exercises,
+        0
+    )
+
+    return (
+        <p>Number of exercises {reducer}</p>
     ) 
 }
 
