4f70fb4dc2f3ea6d0c5a605622fc454b7e57dcbb
kitoshi <kitoshi.charlton@gmail.com>
2021-04-20 11:01:10 -0700

    2.15: Phonebook step7

---
+++ part 2.15/src/App.js
@@ -0,0 +1,114 @@
+import React, { useState, useEffect } from 'react';
+import Add from './components/Add';
+import Filter from './components/Filter';
+import Persons from './components/Persons'
+import axios from 'axios'
+
+const App = () => {
+  const [ persons, setPersons ] = useState([]) 
+  const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
+  const [ newFilter, setNewFilter ] = useState('')
+  
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
+
+  const addAll = (event) =>{
+      addPerson(event)
+      addNumber(event)
+  }
+    
+  
+    const addPerson = (event) => {
+    event.preventDefault()
+    const personsObject = {
+        name: newName,
+        id: persons.length + 1,
+      }
+      const isDuplicate = persons.map(person => person.name)
+      if (isDuplicate.includes(newName)) {
+          return (
+              window.alert(`${newName} is already added to phonebook`)
+            )
+        }
+      else {
+    setPersons(persons.concat(personsObject))
+    setNewName('')
+      }
+    }
+
+    const addNumber = (event) => {
+      event.preventDefault()
+      const numberObject = {
+          name: newName,
+          number: newNumber,
+          id: persons.length + 1,
+        }
+        const isDuplicate = persons.map(person => person.number)
+        if (isDuplicate.includes(newNumber)) {
+            return (
+                window.alert(`${newNumber} is already added to phonebook`)
+              )
+          }
+        else {
+          axios
+          .post('http://localhost:3001/persons', numberObject)
+          .then(response => {
+            setPersons(persons.concat(response.data))
+            setNewNumber('')
+          })
+        }
+      }
+
+      
+      
+
+  
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    setNewFilter(event.target.value)
+  }
+
+  const filterItems = persons.filter(person => person.name.toLowerCase().indexOf(newFilter.toLowerCase()) !== -1)
+  console.log(filterItems)
+
+  const filterList = (filterItems) => {
+    return (
+      <ul key={filterItems.id}>{filterItems.name} {filterItems.number}</ul>
+    )
+  }
+
+  
+
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <Filter
+      handleFilterChange={handleFilterChange} newFilter={newFilter}
+      />
+      <h2>Add a New</h2>
+      <Add 
+      handleNameChange={handleNameChange} handleNumberChange={handleNumberChange} addAll={addAll}
+      />
+      <h2>Numbers</h2>
+      <Persons
+      filterList={filterList} filterItems={filterItems}
+      />
+    </div>
+  )
+  }
+
+export default App
